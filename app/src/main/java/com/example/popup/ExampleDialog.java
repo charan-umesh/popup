9322727d791cc9e8f2819bea3bc7cea148775339
Charan <charanahu605@gmail.com>
2020-03-10 00:03:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/popup/ExampleDialog.java
@@ -0,0 +1,66 @@
+package com.example.popup;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+
+public class ExampleDialog extends AppCompatDialogFragment {
+    private TextView editTextUsername;
+    private EditText editTextPassword;
+    private ExampleDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view)
+                .setTitle("Standard Values")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String username = editTextUsername.getText().toString();
+                        String password = editTextPassword.getText().toString();
+                        listener.applyTexts(username, password);
+                    }
+                });
+
+        editTextUsername = view.findViewById(R.id.originalvalue);
+        editTextPassword = view.findViewById(R.id.newvalue);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (ExampleDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "must implement ExampleDialogListener");
+        }
+    }
+
+    public interface ExampleDialogListener {
+        void applyTexts(String username, String password);
+    }
+}
